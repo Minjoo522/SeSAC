b7619ca4bc8c31156893500a5d50af37b0b8d801
Minjoo <m0522inju@gmail.com>
2023-07-11 22:41:15 +0900

    ♻️ calculator 리팩토링

--- javascript/calculator/calculator.js
+++ javascript/calculator/calculator.js
@@ -1,35 +1,46 @@
 const expression = document.querySelector('.form-control');
 
-function input_expression(char) {
+function inputExpression(char) {
+  if (expression.value === '오류') {
+    expression.value = '';
+  } else {
+    expression.value += char;
+  }
+  checkFirstChar(char);
+  checkLastChar();
+}
+
+// 처음에 0이 입력되지 않았는지 확인
+function checkFirstChar(char) {
+  if (expression.value === '0' || expression.value === null) {
+    expression.value = char === '0' ? '' : char;
+  }
+}
+
+// operator 뒤에 operator가 오지 못하도록 확인
+function checkLastChar() {
   const operators = document.querySelectorAll('.operator');
-  expression.value += char;
-  // 연산자 뒤 연산자 입력 불가하게
-  if (
-    expression.value.endsWith('+') ||
-    expression.value.endsWith('-') ||
-    expression.value.endsWith('*') ||
-    expression.value.endsWith('/')
-  ) {
-    operators.forEach((operator) => operator.setAttribute('disabled', ''));
-  } else if (expression.value == '0' || null) {
-    char == '0' ? (expression.value = '') : (expression.value = char);
-  } else {
-    operators.forEach((operator) => operator.removeAttribute('disabled', ''));
-  }
+  const lastChar = expression.value.slice(-1);
+  const isOperator = ['+', '-', '*', '/'].includes(lastChar);
+
+  operators.forEach((operator) => {
+    if (isOperator) {
+      operator.setAttribute('disabled', '');
+    } else {
+      operator.removeAttribute('disabled');
+    }
+  });
 }
 
-function get_result() {
+function getResult() {
   if (expression.value.endsWith('/0')) {
-    expression.value = '';
-    expression.setAttribute('placeholder', '오류');
-  } else if (expression.value != '') {
-    let result = eval(expression.value, 10);
+    expression.value = '오류';
+  } else if (expression.value) {
+    const result = eval(expression.value);
     expression.value = result;
-  } else {
-    expression.value = '';
   }
 }
 
-function clear_event() {
+function clickClear() {
   expression.value = '';
 }
