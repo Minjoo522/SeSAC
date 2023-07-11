1459e139cfc421dd59303ce9537c511ef4c7ff69
Minjoo <m0522inju@gmail.com>
2023-07-11 19:53:46 +0900

    💬 caculator 업데이트

--- javascript/calculator/calculator.js
+++ javascript/calculator/calculator.js
@@ -1,7 +1,9 @@
 const expression = document.querySelector('.form-control');
+
 function input_expression(char) {
   const operators = document.querySelectorAll('.operator');
   expression.value += char;
+  // 연산자 뒤 연산자 입력 불가하게
   if (
     expression.value.endsWith('+') ||
     expression.value.endsWith('-') ||
@@ -9,6 +11,8 @@ function input_expression(char) {
     expression.value.endsWith('/')
   ) {
     operators.forEach((operator) => operator.setAttribute('disabled', ''));
+  } else if (expression.value == '0' || null) {
+    char == '0' ? (expression.value = '') : (expression.value = char);
   } else {
     operators.forEach((operator) => operator.removeAttribute('disabled', ''));
   }
@@ -18,12 +22,14 @@ function get_result() {
   if (expression.value.endsWith('/0')) {
     expression.value = '';
     expression.setAttribute('placeholder', '오류');
-  } else {
-    let result = eval(expression.value);
+  } else if (expression.value != '') {
+    let result = eval(expression.value, 10);
     expression.value = result;
+  } else {
+    expression.value = '';
   }
 }
 
 function clear_event() {
-  expression.value = '0';
+  expression.value = '';
 }
