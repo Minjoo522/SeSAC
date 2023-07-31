483426631848bd9fe2dede88475ded872580cf6f
Minjoo <m0522inju@gmail.com>
2023-07-31 11:12:41 +0900

    📝 7월 31일 강의 - flask migration

--- python/Flask_login/app.py
+++ python/Flask_login/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 
 """
 LoginManager: 로그인 관리를 담당하는 클래스로, Flask 애플리케이션에서 로그인 기능을 초기화하고 관리하는 역할을 합니다.
@@ -21,6 +22,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # DB 생성
 db = SQLAlchemy(app)
 
+# DB 마이그레이션 모듈 로딩
+migrate = Migrate(app, db)
+
 # Flask-Login 초기화
 login_manager = LoginManager()
 login_manager.login_view = 'login' # 로그인 페이지 URI를 명시
@@ -29,14 +33,17 @@ login_manager.init_app(app)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    # password_hash = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(80))
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        # self.password_hash = generate_password_hash(password)
+        self.password = password
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        # return check_password_hash(self.password_hash, password)
+        return self.password == password
 
 @login_manager.user_loader
 def load_user(user_id):
--- python/Flask_login/lecture_note.md
+++ python/Flask_login/lecture_note.md
@@ -8,3 +8,51 @@
 ## @login_required
 
 - 로그인된 사용자인지 검증하고, 로그인이 되어있지 않으면 자동으로 로그인 페이지로 redirect 시켜줌
+
+# user_loader
+
+- User.get()의 리턴 값인 객체를 추출!
+
+```python
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+```
+
+# migration
+
+- DB의 변경사항을 트랙킹하는 모듈을 추가해 줌
+
+```bash
+pip install Flask-Migrate
+```
+
+```python
+from flask_migrate import Migrate
+
+# DB 마이그레이션 모듈 로딩
+migrate = Migrate(app, db)
+```
+
+```bash
+flask db init
+```
+
+- ⬆️ 최초 1회 실행
+- migrations라는 폴더 생성됨(삭제해도 상관 없고, 다시 필요한 경우 flask db init 실행하면 됨)
+- 일반적으로는 수동으로 함
+
+```bash
+flask db migrate -m "나의 변경 사유"
+flask db upgrade
+```
+
+- 변경 사유 작성
+- 실제 db 업그레이드
+- 데이터베이스 롤백을 위해 flask db downgrade 명령도 사용할 수 있음
+
+```bash
+flask db stamp head
+```
+
+- 구버전을 현재의 최신버전으로 강제 설정
